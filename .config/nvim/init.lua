5899fc37db7a86b9ab708a036598af8b1b2d4b96
archdesk <mstkmp@proton.me>
2025-11-03 19:58:53 -0600

    Modify .gitignore, add nvim/init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,35 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Global Settings
+-- _G.icons = require("utils.icons").icons
+-- _G.utils = require("utils.util")
+vim.g.mapleader = " " 
+vim.g.maplocalleader = " "
+require("config.options") -- Import vim.opt.* settings from lua/config/options.lua
+require("config.autocmd") -- Import custom auto commands from lua/config/autocmd.lua 
+
+-- Setup lazy.nvim
+require("lazy").setup({
+	spec = {
+		{ import = "plugins" }
+	},
+	install = { colorscheme = { "retrobox" } },
+	checker = { enabled = true },
+})
+
+require("config.keymaps") -- Import key-mappings from lua/config/keymaps.lua
