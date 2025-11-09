689df1a4cbf431ad7b7f5f79e1336b5047d32d9f
archdesk <mstkmp@proton.me>
2025-11-08 20:12:12 -0600

    Default plugins/dashboard.lua

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,65 +1,59 @@
 return {
 	"nvimdev/dashboard-nvim",
-	lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
-	opts = function()
-		local logo = [[
-         ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
-         ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
-         ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
-         ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
-         ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║           
-         ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝           
-    ]]
+	event = "VimEnter",
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	config = function()
+		require("dashboard").setup({
+			-- These are top-level options
+			theme = "hyper",
+			disable_move = false,
+			shortcut_type = "letter",
+			shuffle_letter = false,
+			change_to_vcs_root = false,
 
-		logo = string.rep("\n", 8) .. logo .. "\n\n"
+			preview = {
+				command = "",
+				file_path = "",
+				file_height = 0,
+				file_width = 0,
+			},
 
-		local opts = {
-			theme = "doom",
 			hide = {
-				-- this is taken care of by lualine
-				-- enabling this messes up the actual laststatus setting after loading a file
-				statusline = false,
+				statusline = true,
+				tabline = true,
+				winbar = true,
 			},
+
+			-- This 'config' table is passed to the theme ('hyper' in this case)
 			config = {
-				header = vim.split(logo, "\n"),
-        -- stylua: ignore
-        center = {
-          { action = 'lua LazyVim.pick()()',                           desc = " Find File",       icon = " ", key = "f" },
-          { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
-          { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
-          { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
-          { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
-          { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
-          { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
-          { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
-          { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
-        },
-				footer = function()
-					local stats = require("lazy").stats()
-					local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-					return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
-				end,
+				-- 'letter_list' must go IN HERE
+				letter_list = "abcdefghilmnopqrstuvwxyz", -- Default list (a-z, no j, k)
+
+				-- All theme-specific options also go here
+				week_header = {
+					enable = false,
+				},
+				shortcut = {}, -- Add your shortcuts here
+				packages = {
+					enable = true,
+				},
+				project = {
+					enable = true,
+					limit = 8,
+					icon = " ",
+					label = "",
+					action = "Telescope find_files cwd=",
+				},
+				mru = {
+					enable = true,
+					limit = 10,
+					icon = " ",
+					label = "",
+					cwd_only = false,
+				},
+				header = {},
+				footer = {},
 			},
-		}
-
-		for _, button in ipairs(opts.config.center) do
-			button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-			button.key_format = "  %s"
-		end
-
-		-- open dashboard after closing lazy
-		if vim.o.filetype == "lazy" then
-			vim.api.nvim_create_autocmd("WinClosed", {
-				pattern = tostring(vim.api.nvim_get_current_win()),
-				once = true,
-				callback = function()
-					vim.schedule(function()
-						vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
-					end)
-				end,
-			})
-		end
-
-		return opts
+		})
 	end,
 }
